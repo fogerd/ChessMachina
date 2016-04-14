bba89cc7a80dc74c3057da4990e3f10daf7254bd
James Pane-Joyce <james.panejoyce@gmail.com>
2016-04-14 16:03:22 -0400

    I'm a twit and had the scoring inverted

--- src/naive_move_scorer.cpp
+++ src/naive_move_scorer.cpp
@@ -10,29 +10,29 @@ double Naive_Move_Scorer::score_move(const Move& move){
 	Pieces taken=move.taken_piece;
 	switch (taken){
 		case WHITE_PAWN:
-			return 1.0;
-		case WHITE_KNIGHT:
-			return 3.0;
-		case WHITE_BISHOP:
-			return 3.0;
-		case WHITE_ROOK:
-			return 5.0;
-		case WHITE_QUEEN:
-			return 9.0;
-		case WHITE_KING:
-			return 100.0;
-		case BLACK_PAWN:
 			return -1.0;
-		case BLACK_KNIGHT:
+		case WHITE_KNIGHT:
 			return -3.0;
-		case BLACK_BISHOP:
+		case WHITE_BISHOP:
 			return -3.0;
-		case BLACK_ROOK:
+		case WHITE_ROOK:
 			return -5.0;
-		case BLACK_QUEEN:
+		case WHITE_QUEEN:
 			return -9.0;
-		case BLACK_KING:
+		case WHITE_KING:
 			return -100.0;
+		case BLACK_PAWN:
+			return 1.0;
+		case BLACK_KNIGHT:
+			return 3.0;
+		case BLACK_BISHOP:
+			return 3.0;
+		case BLACK_ROOK:
+			return 5.0;
+		case BLACK_QUEEN:
+			return 9.0;
+		case BLACK_KING:
+			return 100.0;
 		default:
 			return 0.0;
 	}
